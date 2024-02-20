efc64d8074cbbc846268e0bb39fc75e98af48f4b
Your Name <you@example.com>
2024-02-20 19:28:00 +0700

    Fix code in login_logout_session

--- controllers/signin/check_signi.controller.php
+++ controllers/signin/check_signi.controller.php
@@ -5,11 +5,11 @@ require_once '../../database/database.php';
 require_once '../../models/signin.model.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $Email = htmlspecialchars($_POST['email']);
-    $PWD = htmlspecialchars($_POST['PWD']);
+    $PWD = htmlspecialchars($_POST['passwd']);
     $user = getUser($Email);
     $imahe = getimage($Email);
     if (count($user) > 0){
-        if(password_verify($password, $user['user_password'])){
+        if(password_verify($PWD, $user['user_password'])){
             $_SESSION['user'] = $user;
             $_SESSION['email'] = $Email;
             $_SESSION['image_url'] = $imahe;
