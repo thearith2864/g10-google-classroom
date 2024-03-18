e84075faa36b9ca6bc7ef53b1b4ae71128646643
Your Name <you@example.com>
2024-03-18 19:31:48 +0700

    [feat] Write code to send a notification to the teacher when a student submit the classwork (G10-137)

--- controllers/student_classworks/submit_classwork.controller.php
+++ controllers/student_classworks/submit_classwork.controller.php
@@ -4,10 +4,14 @@ session_start();
 require "../../database/database.php";
 require "../../models/assignment_model.php";
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require '../../vendor/autoload.php';
+
 if($_SERVER['REQUEST_METHOD'] === "POST"){
     if (isset($_FILES['file']) && isset($_POST['idAS'])){
         $codeclass = $_POST['idCS'];
-        echo $codeclass;
         $idwork = $_POST['idAS'];
         $user_id = $_SESSION['user_id'];
         $fileAssignment = $_FILES['file']['name'];
@@ -23,14 +27,87 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
                 $file_upload_path = '../../assets/files/Assignment_for_students_subment/' . $new_file_name;
                 move_uploaded_file($tmp_name, $file_upload_path);
                 submit_classwork($idwork, $user_id['user_id'], $new_file_name, date('Y-m-d'));
+                
+                if (isset($_SESSION['email']) ) {
+                    $email = $_SESSION['email'];
+                    $user = $_SESSION['user']['user_name'];
+                    $teacher_email = getTeacherEmail($codeclass);
+                    $classwork_name = getClassworkName($idwork);
+                    $url_link = 'http://localhost:8888/submit-form?id='. $idwork . '&codeclass=' . $codeclass ;
+                    
+                    
+                    $mail = new PHPMailer(true);
+            
+                    try {
+                        //Server settings
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = $email;                     //SMTP username
+                        $mail->Password   = 'bfam gtvk xgab kkwv';                               //SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            
+                        //Recipients
+                        $mail->setFrom('hay.sev@student.passerellesnumeriques.org', 'hay');
+                        $mail->addAddress($teacher_email);     //Add a recipient
+            
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'Submit a folder';
+                        $mail->Body    = $user.' is submit a folder into the classwork name '. $classwork_name . '. Click <a href="' . $url_link . '">here</a> to access the link.';                  
+                        $mail->AltBody = $user. ' is submit a folder into the classworkname '. $classwork_name . '. Click <a href="' . $url_link . '">here</a> to access the link.';
+            
+                        $mail->send();
+                        echo 'Message has been sent';
+                    } catch (Exception $e) {
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    }
+                }
                 header('location: /submit-form?id='. $idwork . '&codeclass=' . $codeclass);
             }
         }
     }else{ 
+        $codeclass = $_POST['idCS'];
         $link = $_POST['link'];
         $idwork = $_POST['idAS'];
         $user_id = $_SESSION['user_id'];
         submit_classwork($idwork, $user_id['user_id'], $link, date('Y-m-d'));
+        if (isset($_SESSION['email']) ) {
+            $email = $_SESSION['email'];
+            $user = $_SESSION['user']['user_name'];
+            $teacher_email = getTeacherEmail($codeclass);
+            $classwork_name = getClassworkName($idwork);
+            $url_link = 'http://localhost:8888/submit-form?id='. $idwork . '&codeclass=' . $codeclass ;
+
+            $mail = new PHPMailer(true);
+    
+            try {
+                //Server settings
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'hay.sev@student.passerellesnumeriques.org';                     //SMTP username
+                $mail->Password   = 'bfam gtvk xgab kkwv';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+                //Recipients
+                $mail->setFrom($email, $user);
+                $mail->addAddress($teacher_email);     //Add a recipient
+    
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Submit a link';
+                $mail->Body    = $user.' is submit a link into the classwork name '. $classwork_name. '. Click <a href="' . $url_link . '">here</a> to access the link.';
+                $mail->AltBody = $user.' is submit a link into the classwork name '. $classwork_name. '. Click <a href="' . $url_link . '">here</a> to access the link.';
+    
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
         header('location: /submit-form?id='. $idwork . '&codeclass=' . $codeclass);
         
     }   
