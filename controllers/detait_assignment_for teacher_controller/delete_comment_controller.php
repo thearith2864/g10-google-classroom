e3ab1f10dde97dbab5a45ccd5829ead838ebc6f8
THEARITH HOUY <thearith.houy@pnc.lan>
2024-03-25 08:00:47 +0700

    add logic

--- controllers/detait_assignment_for teacher_controller/delete_comment_controller.php
+++ controllers/detait_assignment_for teacher_controller/delete_comment_controller.php
@@ -9,9 +9,14 @@ if ($_GET['ids']){
     deleteComment($idcomment);
   
     header('Location: /submit-form?id=' . $idassignmet . '&codeclass=' . $codeclass);
-}else{
+}elseif(isset($_GET['user'])){
     $idcomment = $_GET['id'];
     deleteComment($idcomment);
     $user = $_GET['user'];
-    header('Location: /detait_assignment?id=' . $idassignmet . '&codeclass=' . $codeclass . '&user_id=' . $user);
+}
+else{
+    $idcomment = $_GET['id'];
+    deleteComment($idcomment);
+    header('Location: /detait_assignment?id=' . $idassignmet . '&codeclass=' . $codeclass );
+
 }
